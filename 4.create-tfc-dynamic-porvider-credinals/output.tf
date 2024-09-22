d99eb2336193af412d35fd9ddb8c23a0ebcd1d89
sandaraunge27 <sandaraungedu@gmail.com>
2024-09-22 17:15:33 +0900

    prepare output

--- 4.create-tfc-dynamic-porvider-credinals/output.tf
+++ 4.create-tfc-dynamic-porvider-credinals/output.tf
@@ -1,5 +1,5 @@
 output "bound_clain" {
-    value = vault_jwt_auth_backend_role.bound_claims
+    value = vault_jwt_auth_backend_role.example.bound_claims
     description = "vault JWT Auth Backend Role bound_clains"
 }
 
