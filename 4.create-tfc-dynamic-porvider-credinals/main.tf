00e1e52a93ab46bf18b09f01a15d3dca88fe5d8e
sandaraunge27 <sandaraungedu@gmail.com>
2024-09-22 17:01:58 +0900

    4.tfc-dynmaic-provider-credinals

---
+++ 4.create-tfc-dynamic-porvider-credinals/main.tf
@@ -0,0 +1,90 @@
+resource "vault_jwt_auth_backend" "secureops-jwt-backend" {
+    description         = "Demonstration of the Terraform JWT auth backend"
+    path                = "jwt"
+    oidc_discovery_url  = "https://app.terraform.io"
+    bound_issuer        = "https://app.terraform.io"
+}
+
+resource "vault_policy" "admin-policy" {
+  name = "admin-policy"
+
+  policy = <<EOT
+path "auth/*"
+{
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+# Create, update, and delete auth methods
+path "sys/auth/*"
+{
+  capabilities = ["create", "update", "delete", "sudo"]
+}
+# List auth methods
+path "sys/auth"
+{
+  capabilities = ["read"]
+}
+# Enable and manage the key/value secrets engine at `secret/` path
+# List, create, update, and delete key/value secrets
+path "secret/*"
+{
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+# Manage secrets engines
+path "sys/mounts/*"
+{
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+# List existing secrets engines.
+path "sys/mounts"
+{
+  capabilities = ["read"]
+}
+# Manage system backend
+path "db/*"
+{
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+# List existing secrets engines.
+path "db/"
+{
+  capabilities = ["read"]
+}
+path "aws-master-account/" {
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+path "aws-master-account/*" {
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+path "sys/policy/*" {
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+path "sys/policy/" {
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+path "sys/policies/*" {
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+path "sys/policies/" {
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+EOT
+}
+
+resource "vault_jwt_auth_backend" "jwt" {
+  path = "jwt"
+}
+
+resource "vault_jwt_auth_backend_role" "example" {
+  backend         = vault_jwt_auth_backend.secureops-jwt-backend.path
+  role_name       = "vault-jwt-auth-role"
+  token_policies  = vault_policy.admin-policy.name
+
+  bound_audiences = ["vault.workload.identity"]
+  bound_claims_type = "glob"
+  bound_claims = {
+    sub = "organization:ars_secureInfraPrj:project:ars_secureInfraPrj:workspace:*:run_phase:*"
+  }
+  user_claim      = "terraform_full_workspace"
+  role_type       = "jwt"
+  token_ttl       = 1200
+}
---
+++ 4.create-tfc-dynamic-porvider-credinals/output.tf
@@ -0,0 +1,9 @@
+output "bound_clain" {
+    value = vault_jwt_auth_backend_role.bound_claims
+    description = "vault JWT Auth Backend Role bound_clains"
+}
+
+output "role_name" {
+    description = "vault JWT Auth Backend Role role name"
+    value = vault_jwt_auth_backend_role.example.role_name 
+}
